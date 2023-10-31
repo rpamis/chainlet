996b5410987c7984d70f31fc4a15cd11654edc51
benym <benyuanming@gmail.com>
2023-10-31 21:51:08 +0800

    feat: 增加内部策略类枚举实体

---
+++ rpamis-pattern-chain/src/main/java/com/rpamis/pattern/chain/entity/InnerStrategy.java
@@ -0,0 +1,40 @@
+package com.rpamis.pattern.chain.entity;
+
+/**
+ * 内部策略枚举
+ *
+ * @author benym
+ * @date 2023/10/31 21:37
+ */
+public enum InnerStrategy {
+
+    /**
+     * 快速失败策略
+     */
+    FAST_FAILED("fastFailed"),
+    /**
+     * 快速返回策略
+     */
+    FAST_RETURN("fastReturn"),
+    /**
+     * 全执行策略
+     */
+    FULL_EXECUTION("fullExecution");
+
+    /**
+     * 对应SPI code
+     */
+    private String code;
+
+    InnerStrategy(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
