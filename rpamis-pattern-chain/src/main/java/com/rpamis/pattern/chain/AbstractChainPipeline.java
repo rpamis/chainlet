55b1f20ab211aaa245348e038e700981d3ac24ed
benym <benyuanming@gmail.com>
2023-12-19 18:07:06 +0800

    fix: 全执行策略遗漏逻辑补充,清理存储list

--- rpamis-pattern-chain/src/main/java/com/rpamis/pattern/chain/AbstractChainPipeline.java
+++ rpamis-pattern-chain/src/main/java/com/rpamis/pattern/chain/AbstractChainPipeline.java
@@ -194,6 +194,7 @@ public abstract class AbstractChainPipeline<T> implements ChainPipeline<T>, Add<
      */
     @Override
     public void afterHandler() {
+        this.checkResults.clear();
         this.pos = 0;
     }
 
